6329fb40d0990263b5995984fc0d7e8cb70bad45
Sanjeevk8 <137869229+Sanjeevk8@users.noreply.github.com>
2023-11-28 12:25:11 +0000

    variables store

---
+++ .devcontainer/devcontainer/01variables.js
@@ -0,0 +1,9 @@
+const accountId = 11223;
+let accountEmail = "sanjeev@gamil.com";
+var accountPassword = "24563";
+accountCity = "pune"
+let accountState;
+
+// accountId = 2; //not allowed
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
