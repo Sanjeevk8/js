8fa27961d50b8307cf46104a7cdc7e9f2e0899d6
Sanjeevk8 <137869229+Sanjeevk8@users.noreply.github.com>
2023-11-28 12:56:44 +0000

    data types

---
+++ .devcontainer/devcontainer/02dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"//treat all js code as newer version
+
+// alert(3+3);//we are usimg nodejs not browser
+
+let name = "sanjeev";
+let age = 25;
+let isloggedIn = true;
+
+// number => 2 to power 53
+// BigInt
+// String => ""
+// boolean => true or false
+// null => standalone value
+// undefined => value not assign
+// symbol => unique
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
