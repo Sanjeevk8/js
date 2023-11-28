67b610491eb148136f0178efe7361714d148d657
Sanjeevk8 <137869229+Sanjeevk8@users.noreply.github.com>
2023-11-28 15:18:35 +0000

    conversion of datatypes

---
+++ .devcontainer/devcontainer/03conversionorOperation.js
@@ -0,0 +1,23 @@
+let score = true;
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+let isloggedIn = "sanjeev";
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true
+//"" => false
+//"sanjeev" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof (stringNumber));
