03197dec6cb896fcbbb191317b231f3cfc8043ac
Sanjeevk8 <137869229+Sanjeevk8@users.noreply.github.com>
2023-11-29 05:56:28 +0000

    opeartins

--- .devcontainer/devcontainer/03conversionorOperation.js
+++ .devcontainer/devcontainer/03conversionorOperation.js
@@ -19,5 +19,32 @@ let booleanIsLoggedIn = Boolean(isloggedIn)
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof (stringNumber));
+// console.log(stringNumber);
+// console.log(typeof (stringNumber));
+
+
+//************************** operations ***********************************
+
+let value = 3; 
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " sanjeev";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
